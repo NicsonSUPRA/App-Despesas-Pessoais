780b1b65eede24685c25e7975ce45abf1a2a29f4
NicsonSUPRA <nicsoncc1234@gmail.com>
2024-10-07 08:46:04 -0300

    ajustes de responsividades de tamanho utilizando o LayoutBuilder

--- lib/components/chartBar.dart
+++ lib/components/chartBar.dart
@@ -14,43 +14,50 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        FittedBox(child: Text('R\$${value.toStringAsFixed(2)}')),
-        SizedBox(
-          height: 5,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey, width: 1.0),
-                    borderRadius: BorderRadius.circular(5),
-                    color: Color.fromRGBO(220, 220, 220, 1)),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(
+          children: [
+            Container(
+                height: constraints.maxHeight * 0.15,
+                child:
+                    FittedBox(child: Text('R\$${value.toStringAsFixed(2)}'))),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1.0),
+                        borderRadius: BorderRadius.circular(5),
+                        color: Color.fromRGBO(220, 220, 220, 1)),
+                  ),
+                  FractionallySizedBox(
+                    heightFactor: percentage,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Color.fromARGB(255, 189, 107, 203),
+                              width: 1.0),
+                          borderRadius: BorderRadius.circular(5),
+                          color: Theme.of(context).primaryColor),
+                    ),
+                  )
+                ],
               ),
-              FractionallySizedBox(
-                heightFactor: percentage,
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Color.fromARGB(255, 189, 107, 203),
-                          width: 1.0),
-                      borderRadius: BorderRadius.circular(5),
-                      color: Theme.of(context).primaryColor),
-                ),
-              )
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Text(label)
-      ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(label)
+          ],
+        );
+      },
     );
   }
 }
